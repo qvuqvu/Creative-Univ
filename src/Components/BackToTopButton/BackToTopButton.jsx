1a2512996178ca734facf2db4e74d1e003d5a346
ULTIMATE-Mystery <quanghayhay1801@gmail.com>
2022-06-30 12:59:32 +0700

    SocialSidebar

--- src/Components/BackToTopButton/BackToTopButton.jsx
+++ src/Components/BackToTopButton/BackToTopButton.jsx
@@ -10,7 +10,7 @@ function BackToTopButton () {
   }
  
   return (
-    <div className="fixed bottom-[7.5%] right-[3.3%] z-50">
+    <div className="fixed bottom-[7.5%] right-[3.3%] z-10">
     <button onClick = {scrollUp}>
       <IoIosArrowUp className="hover:fill-orange-main scale-[4]" color="gray-900"/>
     </button>
